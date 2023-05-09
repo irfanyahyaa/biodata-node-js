ac42922ec59591efd01d6a6e112bc6fdf18df066
irfanyahyaa <irfanyahyaa@gmail.com>
2023-05-09 22:35:04 +0700

    Initial commit

---
+++ biodata.js
@@ -0,0 +1,6 @@
+exports.biodata = function (nama, tempatLahir, tanggalLahir, alamat) {
+    console.log("Nama: " + nama)
+    console.log("Tempat Lahir: " + tempatLahir)
+    console.log("Tanggal Lahir: " + tanggalLahir)
+    console.log("Alamat: " + alamat)
+};
---
+++ moduleBiodata.js
@@ -0,0 +1,9 @@
+var consoleBiodata = require('./biodata')
+
+var nama = "Irfan Yahya Abdillah";
+var tempatLahir = "Bogor";
+var tanggalLahir = "17 September 1999";
+var alamat = "Jl. Beringin, Bogor"
+
+
+consoleBiodata.biodata(nama, tempatLahir, tanggalLahir, alamat);
